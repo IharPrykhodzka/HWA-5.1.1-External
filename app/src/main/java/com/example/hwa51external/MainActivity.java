86ec41c9e284b79d6fea768484e69610cffba7ad
Ihar <Kvait@bk.ru>
2020-04-10 19:51:13 +0300

    Create normal text file

--- app/src/main/java/com/example/hwa51external/MainActivity.java
+++ app/src/main/java/com/example/hwa51external/MainActivity.java
@@ -1,37 +1,26 @@
 package com.example.hwa51external;
-
-import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int REQUEST_CODE_PERMISSION_WRITE_STORAGE = 10;
     private String FILE_NAME = "text.txt";
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,63 +29,24 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
 
-        int permissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION_WRITE_STORAGE);
-
-        }
-
+        createText();
         initList();
     }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CODE_PERMISSION_WRITE_STORAGE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-
-                }
-        }
-    }
-
     private void initList() {
         final ListView listView = findViewById(R.id.list_view);
         final List<DataItems> dataItemsList = new ArrayList<>();
-
-
-        int indexTitle = 0;
-        int indexSubTitle = 1;
-//        int indexImage = 2;
         int image = R.mipmap.ic_launcher;
 
-        while (true) {
-
-
-            if (stringList().size() < indexSubTitle) {
-                break;
-            } else {
-
-
-                dataItemsList.add(new DataItems(stringList().get(indexTitle), stringList().get(indexSubTitle),
-                        image, false));
-
-                indexTitle = indexTitle + 3;
-                indexSubTitle = indexSubTitle + 3;
-//                indexImage = indexImage + 3;
-            }
+        ArrayList<String> strings = stringList();
+        for (int i = 0; i < strings.size(); i+=2) {
+            dataItemsList.add(new DataItems(strings.get(i), strings.get(i + 1), image, false));
         }
-
         final DataItemsAdapter dataItemsAdapter = new DataItemsAdapter(dataItemsList, this);
         listView.setAdapter(dataItemsAdapter);
-
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
                 Object object = dataItemsList.get(position);
-
                 switch (object.toString()) {
                     case "Записная книжка":
                         Intent intentNotes = new Intent(MainActivity.this, NotesActivity.class);
@@ -120,24 +70,29 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-
                 Object object = dataItemsList.get(position);
-
                 Toast.makeText(MainActivity.this, object.toString(), Toast.LENGTH_LONG).show();
-
                 return false;
             }
         });
+    }
+
+    private void createText() {
+        File textFile = new File(getExternalFilesDir(null), FILE_NAME);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(textFile))) {
+            bufferedWriter.write("Записная книжка,Из задания № 2.2.1,Календарь,Из задания № 2.1.3," +
+                                        "Адресс,Из задания № 2.1.2,Настройки,Из задания № 2.2.2");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private String loadText() {
         StringBuilder result = null;
         Context context = this;
         if (isExternalStorageWritable()) {
-
             File textFile = new File(context.getExternalFilesDir(null), FILE_NAME);
             result = new StringBuilder();
-
             try (BufferedReader bufferedReader = new BufferedReader(new FileReader(textFile))) {
                 String line;
                 while ((line = bufferedReader.readLine()) != null) {
@@ -153,21 +108,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private ArrayList<String> stringList() {
-
         String str = loadText();
         str = str.trim();
         String[] arr = str.split(",");
-
         return new ArrayList<>(Arrays.asList(arr));
     }
 
     public boolean isExternalStorageWritable() {
         String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
+        return Environment.MEDIA_MOUNTED.equals(state);
     }
-
-
 }
